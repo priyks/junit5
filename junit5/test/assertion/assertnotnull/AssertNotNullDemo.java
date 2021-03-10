eb9e2a18275e9e6c132b6a0443f2c3f1995a61da
priyks <priyankakulkarni3431@gmail.com>
2021-03-10 21:16:02 +0530

    assertNotNull with its three overloaded methods

--- junit5/test/assertion/assertnotnull/AssertNotNullDemo.java
+++ junit5/test/assertion/assertnotnull/AssertNotNullDemo.java
@@ -1,58 +1,57 @@
-package assertion.assertnull;
+package assertion.assertnotnull;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 import model.Book;
 import service.BookService;
 /**
- * @author priyankaku
- * This class checks whether the BookService book list contains given book title then it
+ *This class checks whether the BookService book list contains given book title then it
  * returns book otherwise returns null
- * assertNull have the following 3 overloaded methods
+ * assertNotNull have the following 3 overloaded methods
+ * assertNotNull evaluates actualBook is not null if its null then it fails 
+ * @author priyankaku
  *
  */
-public class AssertNullDemo {
+
+public class AssertNotNullDemo {
 
 	@Test
-	public void assertNullWithNoMessage() {
+	public void assertNotNullWithNoMessage() {
 		BookService bookService = new BookService();
 		Book headFirst = new Book(1, "Head First Core Java", "wort");
 		Book springpro = new Book(2, "Spring Pro", "lunina");
 		bookService.addBook(headFirst);
 		bookService.addBook(springpro);
-		Book actualBook=bookService.getBookByName("Pro");
+		Book actualBook=bookService.getBookByName("Spring Pro");
 		//System.out.println(actualBook);
-		assertNull(actualBook);
+		assertNotNull(actualBook);
 	}
 	
 	@Test
-	public void assertNullWithMessage() {
+	public void assertNotNullWithMessage() {
 		BookService bookService = new BookService();
 		Book headFirst = new Book(1, "Head First Core Java", "wort");
 		Book springpro = new Book(2, "Spring Pro", "lunina");
 		bookService.addBook(headFirst);
 		bookService.addBook(springpro);
-		Book actualBook=bookService.getBookByName("Pro");
+		Book actualBook=bookService.getBookByName("Head First Core Java");
 		//System.out.println(actualBook);
-		assertNull(actualBook,"book is not null");
+		assertNotNull(actualBook,"book is null");
 	}
 	/**
-	 * it evaluate if actualBook is null then test passes otherwise it will fails with supplier evaluates lazily
+	 * it evaluate if actualBook is Not null then test passes otherwise it will fails with supplier evaluates lazily
 	 */
 	@Test
-	public void assertNullWithMessageSupplier() {
+	public void assertNotNullWithMessageSupplier() {
 		BookService bookService = new BookService();
 		Book headFirst = new Book(1, "Head First Core Java", "wort");
 		Book springpro = new Book(2, "Spring Pro", "lunina");
 		bookService.addBook(headFirst);
 		bookService.addBook(springpro);
-		Book actualBook=bookService.getBookByName("Pro");
+		Book actualBook=bookService.getBookByName("Spring Pro");
 		//System.out.println(actualBook);
-		assertNull(actualBook,()->"book is not null");
+		assertNotNull(actualBook,()->"book is null");
 	}
 	
-	
-
 }
